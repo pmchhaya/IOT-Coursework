d1f9a1c468756108fa997da0c94b083da35b2294
pmchhaya <91729753+pmchhaya@users.noreply.github.com>
2024-03-16 20:31:08 +0000

    Add files via upload

--- IOT_CWK.ino
+++ IOT_CWK.ino
@@ -1,62 +1,49 @@
-/*
-  Hello World.ino
-  2013 Copyright (c) Seeed Technology Inc.  All right reserved.
-
-  Author:Loovee
-  2013-9-18
-
-  Grove - Serial LCD RGB Backlight demo.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
 #include <Wire.h>
-#include "rgb_lcd.h"
+#include <rgb_lcd.h>
 
 rgb_lcd lcd;
 
-const int colorR = 255;
-const int colorG = 0;
-const int colorB = 0;
+void setup() {
+  // Set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
 
-void setup() 
-{
-    // set up the LCD's number of columns and rows:
-    lcd.begin(16, 2);
-    
-    lcd.setRGB(colorR, colorG, colorB);
-    
-    // Print a message to the LCD.
-    lcd.print("Free legendary!");
-    
+  // Initialize random number generator with an unconnected analog pin
+  randomSeed(analogRead(0));
 
-    delay(1000);
+  // Initial message
+  lcd.setRGB(0, 255, 0); // Set the backlight color to green
+  lcd.print("Getting rarity...");
 }
 
-void loop() 
-{
-    // set the cursor to column 0, line 1
-    // (note: line 1 is the second row, since counting begins with 0):
-    lcd.setCursor(0, 1);
-    lcd.print("Enjoy!:)");
-    // print the number of seconds since reset:
-    //lcd.print(millis()/1000);
+void loop() {
+  // Choose a rarity randomly
+  int rarityIndex = random(0, 4); // Generates a random number between 0 and 3
+  String rarity;
+  switch (rarityIndex) {
+    case 0:
+      rarity = "Legendary";
+      lcd.setRGB(255, 0, 0); // Red for legendary
+      break;
+    case 1:
+      rarity = "Epic";
+      lcd.setRGB(128, 0, 128); // Purple for epic
+      break;
+    case 2:
+      rarity = "Rare";
+      lcd.setRGB(0, 0, 255); // Blue for rare
+      break;
+    case 3:
+      rarity = "Common";
+      lcd.setRGB(255, 255, 255); // White for common
+      break;
+  }
 
-    delay(100);
+  // Clear the LCD and print the winning message
+  lcd.clear();
+  lcd.print("You've won a");
+  lcd.setCursor(0, 1); // Move cursor to the second line
+  lcd.print("free: " + rarity + "!");
+
+  // Wait for a bit before showing the next rarity
+  delay(5000); // Delay for 5 seconds
 }
-
-/*********************************************************************************************************
-  END FILE
-*********************************************************************************************************/
